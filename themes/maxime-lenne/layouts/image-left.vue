a9fe72d666b8fb6b122c429551841a5d5efc5d04
Maxime Lenne <hello@maxime-lenne.fr>
2025-12-10 09:43:47 +0100

    first shoot

---
+++ themes/maxime-lenne/layouts/image-left.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// Image-left layout - image on left, content on right
+</script>
+
+<template>
+  <div class="slidev-layout image-left">
+    <div class="image-container flex items-center justify-center">
+      <slot name="image" />
+    </div>
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ themes/maxime-lenne/layouts/image-right.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// Image-right layout - content on left, image on right
+</script>
+
+<template>
+  <div class="slidev-layout image-right">
+    <div class="content">
+      <slot />
+    </div>
+    <div class="image-container flex items-center justify-center">
+      <slot name="image" />
+    </div>
+  </div>
+</template>
